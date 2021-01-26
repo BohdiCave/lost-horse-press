366adb01753d027a661f3a38d4a725d56c2c88e2
Bohdan Pechenyak <bohdan.pechenyak@gmail.com>
2021-01-26 13:28:14 -0500

    new landing-page background

--- animation.js
+++ animation.js
@@ -26,24 +26,3 @@ newsLink.addEventListener("animationend", () => {
     aboutLink.removeAttribute("style");
     siteMap.removeAttribute("style");
 });
-
-
-// element.addEventListener("animationiteration", listener, false);
-
-// element.className = "slidein";
-
-// function listener(event) {
-    // var l = document.createElement("li");
-//     switch(event.type) {
-//       case "animationstart":
-//         l.textContent = `Started: elapsed time is ${event.elapsedTime}`;
-//         break;
-//       case "animationend":
-//         l.textContent = `Ended: elapsed time is ${event.elapsedTime}`;
-//         break;
-//       case "animationiteration":
-//         l.textContent = `New loop started at time ${event.elapsedTime}`;
-//         break;
-//     }
-//     document.getElementById("output").appendChild(l);
-//   }
