de4f9ee41da4d6db0c15f98a0e1c827013439a4d
Bohdan Pechenyak <bohdan.pechenyak@gmail.com>
2021-02-13 16:27:35 -0500

    bug fix - no display on mobile

--- server.js
+++ server.js
@@ -20,14 +20,21 @@ const PORT = process.env.PORT || 3001;
 // middleware 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// static assets (on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+
 // routes, both API and view
 app.use(router);
 app.use(passport.initialize());
 require('./config/passport')(passport);
+
+// static assets (on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.get('/*', (req, res, next) => {
+  res.setHeader('Last-Modified', (newDate()).toUTCString());
+  next();
+});
 // Start the API server
 if (process.env.JAWSDB_URL) {
   db.sync({force:false}).then(() => {
