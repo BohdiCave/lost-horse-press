5fbb8e145e076040efe031f3048a82fb98fbbfc0
Bohdan Pechenyak <bohdan.pechenyak@gmail.com>
2021-03-01 21:12:39 -0500

    modal routing fix

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import '../styles/modal.css';
 import {FaNewspaper, FaListOl, FaShoppingCart, FaAward, FaInfoCircle, FaBullhorn} from "react-icons/fa";
 import {MdNewReleases, MdContactPhone} from "react-icons/md/";
@@ -12,7 +13,7 @@ export default function Modal({ handleClose, show }) {
     <div className={showHideClassName}>
       <section className="modal-main">
         <div className="close-btn">
-          <a href="/"><img src="./assets/images/lhp_logo_250x200.png" id="side-logo" alt="logo"/></a>
+          <Link to="/"><img src="./assets/images/lhp_logo_250x200.png" id="side-logo" alt="logo"/></Link>
           <button id="modal-close" type="button" onClick={handleClose}>
             Close Map
           </button>
@@ -20,31 +21,31 @@ export default function Modal({ handleClose, show }) {
         <div className="site-map">
           <span>NEWS</span>
           <ul className="site-map-menu" id="newsMenu">
-            <li><a href="/home"><FaNewspaper className="icon1" /> Latest News</a></li>
-            <li><a href="/home"><MdNewReleases className="icon2"/>New Releases</a></li>  
+            <li><Link to="/home"><FaNewspaper className="icon1" /> Latest News</Link></li>
+            <li><Link to="/home"><MdNewReleases className="icon2"/>New Releases</Link></li>  
           </ul>
         </div>
         <div className="site-map">
           <span>BOOK CATALOG</span>
           <ul id="catMenu" className="site-map-menu">  
-            <li><a href="/catalog"><IoLibrarySharp className="icon1" /> Listings</a></li>
-            <li><a href="/series"><FaListOl className="icon2" /> Series</a></li>
-            <li><a href="/order"><FaShoppingCart className="icon3" /> Order</a></li>
+            <li><Link to="/catalog"><IoLibrarySharp className="icon1" /> Listings</Link></li>
+            <li><Link to="/series"><FaListOl className="icon2" /> Series</Link></li>
+            <li><Link to="/order"><FaShoppingCart className="icon3" /> Order</Link></li>
           </ul>
         </div>
         <div className="site-map">
         <span>SUBMISSIONS</span>
           <ul className="site-map-menu" id="subsMenu">
-            <li><a href="/submissions"><IoSendSharp className="icon1" /> Submissions</a></li>
-            <li><a href="/awards"><FaAward className="icon2" /> Poetry Prize</a></li>
+            <li><Link to="/submissions"><IoSendSharp className="icon1" /> Submissions</Link></li>
+            <li><Link to="/awards"><FaAward className="icon2" /> Poetry Prize</Link></li>
           </ul>
         </div>
         <div className="site-map">
           <span>ABOUT</span>
           <ul id="aboutMenu" className="site-map-menu" >
-              <li><a href="/about"><FaInfoCircle className="icon1" /> History</a></li>
-              <li><a href="/about#about-mission"><FaBullhorn className="icon2" /> Mission</a></li>
-              <li><a href="/about#about-contacts"><MdContactPhone className="icon3" />Contacts</a></li>
+              <li><Link to="/about"><FaInfoCircle className="icon1" /> History</Link></li>
+              <li><Link to="/about#about-mission"><FaBullhorn className="icon2" /> Mission</Link></li>
+              <li><Link to="/about#about-contacts"><MdContactPhone className="icon3" />Contacts</Link></li>
           </ul>
         </div>
        
